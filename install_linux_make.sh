570c7c3918053611851cdb7c748dc1e4c99dc7f0
Patrik Huber <patrikhuber@gmail.com>
2012-07-12 17:04:31 +0200

    Install-scripts
    Mainly for linux "Make", and to not have to remember all the commands.
    Not finished.

---
+++ install_linux_make.sh
@@ -0,0 +1,4 @@
+echo "This sets up a Debug and Release build for use with linux Makefiles. If you are using another CMake generator (e.g. to build Eclipse project files), don't use this."
+echo "Directories for Debug and Release builds are now ready."
+echo "Run 'make Debug', 'make Release' or just 'make' to build both."
+echo "After that, CMake created makefiles for you in ... cd into and type ."
---
+++ install_win.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+echo No setup necessary. Just run
+echo ----------------------------
+echo cmake -C ..\FeatureDetection\initial_cache.cmake ..\FeatureDetection
+echo ----------------------------
+echo from the build directory (you should currently be in the build directory).
