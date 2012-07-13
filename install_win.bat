7e7048d31662ab88ea115df211c577f841ff0ce4
Patrik Huber <patrikhuber@gmail.com>
2012-07-14 00:08:30 +0200

    win-install bat update

--- install_win.bat
+++ install_win.bat
@@ -1,6 +1,18 @@
 @ECHO OFF
+echo.
+echo 1) INSTALL:
+echo ============================
 echo No setup necessary. Just run
-echo ----------------------------
+echo ---
 echo cmake -C ..\FeatureDetection\initial_cache.cmake ..\FeatureDetection
-echo ----------------------------
+echo ---
 echo from the build directory (you should currently be in the build directory).
+echo.
+echo 2) COMPILE:
+echo ============================
+echo Open the .sln and compile in Visual Studio.
+echo.
+echo 3) RUN:
+echo ============================
+echo To run the apps, you should add the following paths (please change it to reflect your paths) to your windows PATH variable:
+echo D:\ocvsvnbin\bin\Release;D:\ocvsvnbin\bin\Debug;D:\boost\boost_1_47\lib;C:\Program Files (x86)\MATLAB\R2012a\bin\win32
