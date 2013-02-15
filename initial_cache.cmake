fd0ec77219fb019e3b67671f88aa84b36252aa0a
Patrik Huber <patrikhuber@gmail.com>
2013-02-15 22:44:44 +0100

    - Folder rename
    - CMakeLists update

--- initial_cache.cmake
+++ initial_cache.cmake
@@ -1,7 +1,7 @@
 # Mechanism via FindLIB.cmake
 # ==============================
 # In windows, this HAS TO BE set. In Linux, if boost is installed system-wide, it's not necessary. Just leave it (should work), or uncomment.
-#set( BOOST_ROOT     "C:\\boost\\install"    CACHE STRING "Boost search location" ) # Windows, self-built
+set( BOOST_ROOT     "D:\\boost\\install"    CACHE STRING "Boost search location" ) # Windows, self-built
 #set( BOOST_ROOT     "/home/patrik/boost/install"    CACHE STRING "Boost search location" ) # Linux, self-built
 
 # This CAN be set if the automatic script fails, but usually it works:
@@ -13,5 +13,5 @@
 # Mechanism via ConfigLIB.cmake in lib dir
 # ==============================
 # In windows, this HAS TO BE set. In Linux, it's usually not necessary. Just leave it (should work), or uncomment.
-set( OpenCV_DIR   "C:\\opencvgit\\install"   CACHE STRING "OpenCV config dir, where OpenCVConfig.cmake can be found" ) # Windows, self-built
+set( OpenCV_DIR   "D:\\opencv\\install"   CACHE STRING "OpenCV config dir, where OpenCVConfig.cmake can be found" ) # Windows, self-built
 #set( OpenCV_DIR   "/home/patrik/opencv/install/share/OpenCV"   CACHE STRING "OpenCV config dir, where OpenCVConfig.cmake can be found" ) # Linux, openCV from git (self-built)
