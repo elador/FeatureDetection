a013be82b83d2b3def82c1a40faa503eecfedb65
Patrik Huber <patrikhuber@gmail.com>
2014-04-25 09:21:01 +0200

    Added an example to the FR-curve scripts

--- frDbExp/share/scripts/plotAllExperiments.m
+++ frDbExp/share/scripts/plotAllExperiments.m
@@ -1,15 +1,12 @@
 clear;
 close all;
 
-plotCurves('C:\Users\Patrik\Documents\MATLAB\probes_pm15_fte.txt', 'r', 0, 1); % 1 = plot with FTEs, 1 = plotIndividualDataPoints
-%plotCurves('C:\Users\Patrik\Documents\MATLAB\probes_pm30_fte.txt', 'b', 0, 1);
-%plotCurves('C:\Users\Patrik\Documents\MATLAB\probes_pm45_fte.txt', 'g', 0, 1);
-%plotCurves('C:\Users\Patrik\Documents\MATLAB\probes_pm60_fte.txt', 'y', 0, 1);
-
-%plotCurves('C:\Users\Patrik\Documents\MATLAB\probes_all.txt', 'r', 0, 1); % add parameter verifRateAtFAR
+% Arguments: filename, curveColor, plotWithFtes, plotIndividualDataPoints
+plotCurves('example_input.txt', 'r', 0, 1);
 
 
 % Todo:
 % -- the numbers: 1) ident. rate 2) ver. rate at x% far (oä)
 % -- bei 15°, die DET kurve bzw die vektoren sollten doch egtl 0 elemente
 % enthalten, nur ein punkt bei 0, 0 ?
+% -- plotCurves: maybe add parameter verifRateAtFAR?
