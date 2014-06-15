cb596e455dab2dddde8425ecbc4fdc14299bf440
Patrik Huber <patrikhuber@gmail.com>
2014-06-15 11:22:17 +0100

    Added a libFaceRecognition to contain utility-stuff for performing recognition experiments

---
+++ libFaceRecognition/src/facerecognition/FaceRecord.cpp
@@ -0,0 +1,14 @@
+/*
+ * FaceRecord.cpp
+ *
+ *  Created on: 14.06.2014
+ *      Author: Patrik Huber
+ */
+
+#include "facerecognition/FaceRecord.hpp"
+
+#include "logging/LoggerFactory.hpp"
+
+namespace facerecognition {
+
+} /* namespace facerecognition */
