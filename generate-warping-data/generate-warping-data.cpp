f46afe928e819bc2c6d7b96401911fa67dc7b9fe
Patrik Huber <patrikhuber@gmail.com>
2014-08-11 00:44:24 +0100

    Removed more deprecated stuff from generate-warping-data, plus a few minor changes

--- generate-warping-data/generate-warping-data.cpp
+++ generate-warping-data/generate-warping-data.cpp
@@ -21,13 +21,10 @@
 #include "render/SoftwareRenderer.hpp"
 #include "render/Camera.hpp"
 #include "render/utils.hpp"
-
 #include "morphablemodel/MorphableModel.hpp"
-
 #include "imageio/LandmarkCollection.hpp"
 #include "imageio/ModelLandmark.hpp"
 #include "imageio/DidLandmarkFormatParser.hpp"
-
 #include "logging/LoggerFactory.hpp"
 
 #include "opencv2/core/core.hpp"
@@ -71,44 +68,6 @@ std::ostream& operator<<(std::ostream& os, const vector<T>& v)
 	return os;
 }
 
-static string windowName = "test";
-static float horizontalAngle = 0.0f;
-static float verticalAngle = 0.0f;
-static int lastX = 0;
-static int lastY = 0;
-static float movingFactor = 0.5f;
-static bool moving = false;
-
-static float zNear = 0.01f;
-static float zFar = 100.0f;
-static bool perspective = false;
-static bool freeCamera = true;
-
-static void winOnMouse(int event, int x, int y, int, void* userdata)
-{
-	if (event == cv::EVENT_MOUSEMOVE && moving == false) {
-		lastX = x;
-		lastY = y;
-	}
-	if (event == cv::EVENT_MOUSEMOVE && moving == true) {
-		if (x != lastX || y != lastY) {
-			horizontalAngle += (x-lastX)*movingFactor;
-			verticalAngle += (y-lastY)*movingFactor;
-			lastX = x;
-			lastY = y;
-		}
-	}
-	if (event == cv::EVENT_LBUTTONDOWN) {
-		moving = true;
-		lastX = x;
-		lastY = y;
-	}
-	if (event == cv::EVENT_LBUTTONUP) {
-		moving = false;
-	}
-
-}
-
 int main(int argc, char *argv[])
 {
 	#ifdef WIN32
@@ -126,8 +85,8 @@ int main(int argc, char *argv[])
 				"produce help message")
 			("verbose,v", po::value<string>(&verboseLevelConsole)->implicit_value("DEBUG")->default_value("INFO", "show messages with INFO loglevel or below."),
 				"specify the verbosity of the console output: PANIC, ERROR, WARN, INFO, DEBUG or TRACE")
-			("config,c", po::value<path>(&configFilename)->required(),
-				"Path to a config file that specifies which Morphable Model to load.")
+			("model,m", po::value<path>(&configFilename)->required(),
+				"path to a config file that specifies which Morphable Model to load")
 		;
 
 		po::variables_map vm;
@@ -157,10 +116,11 @@ int main(int argc, char *argv[])
 		return EXIT_SUCCESS;
 	}
 
+	Loggers->getLogger("render").addAppender(make_shared<logging::ConsoleAppender>(logLevel));
+	Loggers->getLogger("morphablemodel").addAppender(make_shared<logging::ConsoleAppender>(logLevel));
 	Loggers->getLogger("generate-warping-data").addAppender(make_shared<logging::ConsoleAppender>(logLevel));
 	Logger appLogger = Loggers->getLogger("generate-warping-data");
 	appLogger.debug("Verbose level for console output: " + logging::logLevelToString(logLevel));
-
 
 	ptree pt;
 	try {
@@ -171,12 +131,8 @@ int main(int argc, char *argv[])
 		return EXIT_FAILURE;
 	}
 
-	//mm = shapemodels::MorphableModel::loadScmModel("C:\\Users\\Patrik\\Documents\\GitHub\\bsl_model_first\\SurreyLowResGuosheng\\NON3448\\ShpVtxModelBin_NON3448.scm", "C:\\Users\\Patrik\\Documents\\GitHub\\featurePoints_SurreyScm.txt");
 	//mm = shapemodels::MorphableModel::loadOldBaselH5Model("C:\\Users\\Patrik\\Documents\\GitHub\\bsl_model_first\\model2012p.h5", "featurePoints_head_newfmt.txt");
 	//mm = shapemodels::MorphableModel::loadStatismoModel("C:\\Users\\Patrik\\Documents\\GitHub\\bsl_model_first\\2012.2\\head\\model2012_l7_head.h5");
-	//shapemodels::MorphableModel morphableModel = shapemodels::MorphableModel::loadScmModel("C:\\Users\\Patrik\\Cloud\\PhD\\MorphModel\\ShpVtxModelBin.scm", "C:\\Users\\Patrik\\Documents\\GitHub\\featurePoints_SurreyScm.txt");
-	//shapemodels::MorphableModel morphableModel = shapemodels::MorphableModel::loadScmModel(".\\ShpVtxModelBin.scm", ".\\featurePoints_SurreyScm.txt");
-	
 	morphablemodel::MorphableModel morphableModel;
 	try {
 		morphableModel = morphablemodel::MorphableModel::load(pt.get_child("morphableModel"));
@@ -198,21 +154,18 @@ int main(int argc, char *argv[])
 	SoftwareRenderer renderer(screenWidth, screenHeight);
 	renderer.doBackfaceCulling = true;
 
-	//namedWindow(windowName, WINDOW_AUTOSIZE);
-	//setMouseCallback(windowName, winOnMouse);
-
 	vector<int> vertexIds; // Todo: Work with the tlms names everywhere instead of the vertex indices
 	/* Surrey full model: */
-	vertexIds.push_back(11389); // 177: left-eye-left - right.eye.corner_outer
-	vertexIds.push_back(25864/*10930*/); // 181: left-eye-right - right.eye.corner_inner
-	vertexIds.push_back(25868); // 614: right-eye-left - left.eye.corner_inner. But we take one a little bit more up
-	vertexIds.push_back(11395); // 610: right-eye-right - left.eye.corner_outer
+//	vertexIds.push_back(11389); // 177: left-eye-left - right.eye.corner_outer
+//	vertexIds.push_back(25864/*10930*/); // 181: left-eye-right - right.eye.corner_inner
+//	vertexIds.push_back(25868); // 614: right-eye-left - left.eye.corner_inner. But we take one a little bit more up
+//	vertexIds.push_back(11395); // 610: right-eye-right - left.eye.corner_outer
 	vertexIds.push_back(398); // mouth-left - right.lips.corner
 	vertexIds.push_back(812); // mouth-right - left.lips.corner
-	vertexIds.push_back(11140); // bridge of the nose - // should use the new one from the reference, MnFMdl.obj
+//	vertexIds.push_back(11140); // bridge of the nose - // should use the new one from the reference, MnFMdl.obj
 	vertexIds.push_back(114); // nose-tip - center.nose.tip
 	vertexIds.push_back(270); // nasal septum - 
-	vertexIds.push_back(3284); // left-alare - right nose ...
+//	vertexIds.push_back(3284); // left-alare - right nose ...
 	vertexIds.push_back(572); // right-alare - left nose ...
 	
 	// BFM statismo:
@@ -230,7 +183,7 @@ int main(int argc, char *argv[])
 	*/
 
 	std::ofstream outputFile;
-	outputFile.open("./data_3dmm_landmarks_random_sd0.7_batch6_1000k.txt"); // data_3dmm_landmarks_random_sd0.5_batch4_600000k.txt
+	outputFile.open("./data_3dmm_landmarks_random_sd0.7_batch7_1000k_test.txt"); // data_3dmm_landmarks_random_sd0.5_batch4_600000k.txt
 	outputFile << "frontal_rndvtx_x " << "frontal_rndvtx_y " << "frontal_lel_x " << "frontal_lel_y " << "frontal_ler_x " << "frontal_ler_y " << "frontal_rel_x " << "frontal_rel_y " << "frontal_rer_x " << "frontal_rer_y " << "frontal_ml_x " << "frontal_ml_y " << "frontal_mr_x " << "frontal_mr_y " << "frontal_bn_x " << "frontal_bn_y " << "frontal_nt_x " << "frontal_nt_y " << "frontal_ns_x " << "frontal_ns_y " << "frontal_la_x " << "frontal_la_y " << "frontal_ra_x " << "frontal_ra_y " << "pose_rndvtx_x " << "pose_rndvtx_y " << "pose_lel_x " << "pose_lel_y " << "pose_ler_x " << "pose_ler_y " << "pose_rel_x " << "pose_rel_y " << "pose_rer_x " << "pose_rer_y " << "pose_ml_x " << "pose_ml_y " << "pose_mr_x " << "pose_mr_y " << "pose_bn_x " << "pose_bn_y " << "pose_nt_x " << "pose_nt_y " << "pose_ns_x " << "pose_ns_y " << "pose_la_x " << "pose_la_y " << "pose_ra_x " << "pose_ra_y " << "yaw " << "pitch " << "roll " << "rndvtx_id" << std::endl;
 	
 	int numVertices = morphableModel.getShapeModel().getDataDimension() / 3;
@@ -273,12 +226,12 @@ int main(int argc, char *argv[])
 		// 1) Render the randomVertex frontal
 		Mat modelMatrix = Mat::eye(4, 4, CV_32FC1);
 		Mat colorbuffer, depthbuffer;
-		Vec3f res = render::utils::projectVertex(newSampleMesh.vertex[randomVertex].position, modelMatrix, screenWidth, screenHeight);
+		Vec3f res = render::utils::projectVertex(newSampleMesh.vertex[randomVertex].position, camera.getProjectionMatrix() * camera.getViewMatrix() * modelMatrix, screenWidth, screenHeight);
 		renderer.clearBuffers();
 		std::tie(colorbuffer, depthbuffer) = renderer.render(newSampleMesh, camera.getViewMatrix() * modelMatrix, camera.getProjectionMatrix());
 		string name = "randomVertexFrontal";
 		pointsToWrite.push_back(make_shared<ModelLandmark>(name, res));
-		cv::circle(colorbuffer, cv::Point(res[0], res[1]), 3, cv::Scalar(0, 0, 255));
+		cv::circle(colorbuffer, cv::Point(res[0], res[1]), 3, cv::Scalar(0, 0, 255, 255));
 
 		// 2) Render all LMs in frontal pose
 		for (const auto& vid : vertexIds) {
@@ -287,7 +240,7 @@ int main(int argc, char *argv[])
 			//r.renderLM(newSampleMesh.vertex[vid].position, Scalar(255.0f, 0.0f, 0.0f));
 			name = DidLandmarkFormatParser::didToTlmsName(vid);
 			pointsToWrite.push_back(make_shared<ModelLandmark>(name, res));
-			cv::circle(colorbuffer, cv::Point(res[0], res[1]), 3, cv::Scalar(255, 0, 128));
+			cv::circle(colorbuffer, cv::Point(res[0], res[1]), 3, cv::Scalar(255, 0, 128, 255));
 		}
 		imwrite("out/" + lexical_cast<string>(cnt)+"_front.png", colorbuffer);
 
@@ -297,20 +250,20 @@ int main(int argc, char *argv[])
 		rotRollZ = render::matrixutils::createRotationMatrixZ(roll * (CV_PI / 180.0f));
 		modelMatrix = rotYawY * rotPitchX * rotRollZ;
 		renderer.clearBuffers();
-		auto framebuffersPose = renderer.render(newSampleMesh, camera.getViewMatrix() * modelMatrix, camera.getProjectionMatrix());
+		std::tie(colorbuffer, depthbuffer) = renderer.render(newSampleMesh, camera.getViewMatrix() * modelMatrix, camera.getProjectionMatrix());
 
 		res = render::utils::projectVertex(newSampleMesh.vertex[randomVertex].position, camera.getProjectionMatrix() * camera.getViewMatrix() * modelMatrix, screenWidth, screenHeight);
-		double zBufferValue = framebuffersPose.second.at<double>(static_cast<int>(cvRound(res[1])), static_cast<int>(cvRound(res[0])));
+		double zBufferValue = depthbuffer.at<double>(static_cast<int>(cvRound(res[1])), static_cast<int>(cvRound(res[0])));
 		cv::Point2i centerPixel(floor(res[0]), floor(res[1]));
 		int minzx = std::max(0, centerPixel.x - 1);
-		int maxzx = std::min(centerPixel.x + 1, framebuffersPose.second.cols - 1);
+		int maxzx = std::min(centerPixel.x + 1, depthbuffer.cols - 1);
 		int minzy = std::max(0, centerPixel.y - 1);
-		int maxzy = std::min(centerPixel.y + 1, framebuffersPose.second.rows - 1);
+		int maxzy = std::min(centerPixel.y + 1, depthbuffer.rows - 1);
 		bool isVisible = false;
 		for (int x = minzx; x < maxzx; ++x) {
 			for (int y = minzy; y < maxzy; ++y) {
-				double zBufferValue = framebuffersPose.second.at<double>(y, x);
-				if (res[2] <= zBufferValue) {
+				double zBufferValue = depthbuffer.at<double>(y, x);
+				if (res[2] <= zBufferValue) { // swap sign?
 					isVisible = true;
 				}
 			}
@@ -319,11 +272,11 @@ int main(int argc, char *argv[])
 		//if (res[2] > zBufferValue + 0.00004) { // we apply a threshold because our projectVertex is somehow a little bit off, probably because rasterTriangle() works a bit different? (offset, rounding, ...).
 			// But caution, this hack depends on the resolution of the z-buffer?
 			// not visible
-			cv::circle(framebuffersPose.first, cv::Point(res[0], res[1]), 3, cv::Scalar(255, 0, 128));
-			imwrite("out/" + lexical_cast<string>(cnt)+"_pose_invis.png", framebuffersPose.first);
+			cv::circle(colorbuffer, cv::Point(res[0], res[1]), 3, cv::Scalar(255, 0, 128, 255));
+			imwrite("out/" + lexical_cast<string>(cnt)+"_pose_invis.png", colorbuffer);
 			continue;
 		}
-		cv::circle(framebuffersPose.first, cv::Point(res[0], res[1]), 3, cv::Scalar(255, 0, 128));
+		cv::circle(colorbuffer, cv::Point(res[0], res[1]), 3, cv::Scalar(255, 0, 128, 255));
 		name = "randomVertexPose";
 		pointsToWrite.push_back(make_shared<ModelLandmark>(name, res));
 
@@ -333,9 +286,9 @@ int main(int argc, char *argv[])
 			res = render::utils::projectVertex(newSampleMesh.vertex[vid].position, camera.getProjectionMatrix() * camera.getViewMatrix() * modelMatrix, screenWidth, screenHeight);
 			name = DidLandmarkFormatParser::didToTlmsName(vid);
 			pointsToWrite.push_back(make_shared<ModelLandmark>(name, res));
-			cv::circle(framebuffersPose.first, cv::Point(res[0], res[1]), 3, cv::Scalar(128, 0, 255));
+			cv::circle(colorbuffer, cv::Point(res[0], res[1]), 3, cv::Scalar(128, 0, 255, 255));
 		}
-		imwrite("out/" + lexical_cast<string>(cnt)+"_pose_vis.png", framebuffersPose.first);
+		imwrite("out/" + lexical_cast<string>(cnt)+"_pose_vis.png", colorbuffer);
 					
 		// 4) Write one row to the file
 		for (const auto& lm : pointsToWrite) {
