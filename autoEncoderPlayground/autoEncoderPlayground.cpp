9df4cef3fef74e4aea34ccd87a925686db62c40a
Patrik Huber <patrikhuber@gmail.com>
2014-09-17 23:57:23 +0200

    Finished the AutoEncoder cost function

--- autoEncoderPlayground/autoEncoderPlayground.cpp
+++ autoEncoderPlayground/autoEncoderPlayground.cpp
@@ -94,7 +94,7 @@ std::vector<cv::Mat> generatePatches(std::vector<cv::Mat> images, int patchsize,
 
 	std::random_device rd; ///< Used for seeding the PRNG (mt19937)
 	std::mt19937 engine(rd()); ///< A Mersenne twister mt19937 engine
-	std::uniform_int_distribution<> randomImage(0, images.size() - 1);
+	std::uniform_int_distribution<> randomImage(0, static_cast<int>(images.size()) - 1);
 	std::uniform_int_distribution<> randomPointX(0, imageWidth - patchsize - 1); // maybe -1 or 0 or +1?
 	std::uniform_int_distribution<> randomPointY(0, imageHeight - patchsize -1);
 
@@ -129,7 +129,7 @@ std::vector<cv::Mat> normalizeData(std::vector<cv::Mat> features)
 
 	// Truncate to + / -3 standard deviations and scale to - 1 to 1. stddev of ALL patches together.
 	//pstd = 3 * std(patches(:));
-	Mat allPatches(features[0].rows * features[0].cols, features.size(), CV_32FC1); // each column is 1 patch
+	Mat allPatches(features[0].rows * features[0].cols, static_cast<int>(features.size()), CV_32FC1); // each column is 1 patch
 	for (auto i = 0; i < features.size(); ++i) {
 		Mat reshaped = features[i].reshape(1, features[0].rows * features[0].cols);
 		reshaped.copyTo(allPatches.col(i));
@@ -137,7 +137,7 @@ std::vector<cv::Mat> normalizeData(std::vector<cv::Mat> features)
 	cv::Scalar stddev;
 	cv::Scalar mean;
 	cv::meanStdDev(allPatches, mean, stddev);
-	float pstd = 3 * stddev[0];
+	float pstd = 3.0f * static_cast<float>(stddev[0]);
 	//patches = max(min(patches, pstd), -pstd) / pstd; // % truncate each element individually
 	allPatches = cv::max(cv::min(allPatches, pstd), -pstd) / pstd;
 	// Rescale from[-1, 1] to[0.1, 0.9]
@@ -153,7 +153,7 @@ std::vector<cv::Mat> normalizeData(std::vector<cv::Mat> features)
 cv::Mat initializeParameters(int hiddenSize, int visibleSize)
 {
 	// Initialize parameters randomly based on layer sizes.
-	float r = sqrt(6) / static_cast<float>(sqrt(hiddenSize + visibleSize + 1));   // we'll choose weights uniformly from the interval [-r, r]
+	float r = sqrt(6.0f) / static_cast<float>(sqrt(hiddenSize + visibleSize + 1));   // we'll choose weights uniformly from the interval [-r, r]
 	std::random_device rd; ///< Used for seeding the PRNG (mt19937)
 	std::mt19937 engine(rd()); ///< A Mersenne twister mt19937 engine
 	std::uniform_real_distribution<float> randomNumber(-r, r);
@@ -186,8 +186,54 @@ cv::Mat initializeParameters(int hiddenSize, int visibleSize)
 	return theta;
 }
 
-// visibleSize: the number of input units(probably 64)
-// hiddenSize : the number of hidden units(probably 25)
+template<class T>
+T sigmoid(T val) {
+	return 1 / (1 + std::exp(-val));
+};
+
+// element-wise sigmoid
+// for CV32FC1 only!
+template<>
+cv::Mat sigmoid<cv::Mat>(cv::Mat val)
+{
+	if (val.type() != CV_32FC1) {
+		throw std::runtime_error("Not CV_32FC1.");
+	}
+	Mat result(val.rows, val.cols, val.type());
+	for (decltype(val.rows) r = 0; r < val.rows; ++r) {
+		for (decltype(val.cols) c = 0; c < val.cols; ++c) {
+			// Not sure how we can make the .at<float> call more generic
+			// because the type is encoded in CV_32FC1.
+			result.at<float>(r, c) = sigmoid(val.at<float>(r, c));
+		}
+	}
+	return result;
+}
+
+// J is the cost function
+// theta is the parameter vector we want to optimise (?)
+template<class F>
+cv::Mat computeNumericalGradient(F J, cv::Mat theta)
+{
+	Mat numgrad = Mat::zeros(theta.rows, theta.cols, theta.type()); // will be e.g. 3000 x 1 normally. Make sure it is, we will fail later otherwise?
+	float epsilon = 0.0001f; // 1e-4;
+	int n = theta.rows;
+	Mat E = Mat::eye(n, n, CV_32FC1);
+
+	for (int i = 0; i < n; ++i) {
+		Mat delta = E.col(i) * epsilon;
+		// for every parameter in theta, we compute the  numerical gradient
+		// so basically for every (value +- delta), we compute the whole
+		// neuralnet and its cost.
+		// .first because we want the cost (it returns a pair(cost, grad))
+		numgrad.at<float>(i) = (J(theta + delta).first - J(theta - delta).first) / (2.0f * epsilon);
+	}
+
+	return numgrad;
+}
+
+// visibleSize: the number of input units (probably 64)
+// hiddenSize : the number of hidden units (probably 25)
 // lambda : weight decay parameter
 // sparsityParam : The desired average activation for the hidden units(denoted in the lecture
 //                           notes by the greek alphabet rho, which looks like a lower - case "p").
@@ -198,9 +244,88 @@ cv::Mat initializeParameters(int hiddenSize, int visibleSize)
 // We first convert theta to the(W1, W2, b1, b2) matrix / vector format, so that this
 // follows the notation convention of the lecture notes.
 // return: [cost, grad]
-std::pair<float, float> sparseAutoencoderCost(cv::Mat theta, int visibleSize, int hiddenSize, float lambda, float sparsityParam, float beta, std::vector<cv::Mat> patches)
+std::pair<float, cv::Mat> sparseAutoencoderCost(cv::Mat theta, int visibleSize, int hiddenSize, float lambda, float sparsityParam, float beta, cv::Mat data)
 {
-	return std::make_pair(0.0f, 0.0f);
+	// unroll the parameter-vector:
+	Mat W1 = theta.rowRange(0, 25 * 64).reshape(0, 25);
+	Mat W2 = theta.rowRange(25 * 64, 25 * 64 * 2).reshape(0, 64);
+	Mat b1 = theta.rowRange(25 * 64 * 2, 25 * 64 * 2 + 25);
+	Mat b2 = theta.rowRange(25 * 64 * 2 + 25, 25 * 64 * 2 + 25 + 64);
+
+	float Jcost = 0.0f;
+	float Jweight = 0.0f;
+	float Jsparse = 0.0f;
+
+	auto dataDim = data.rows;
+	auto numExamples = data.cols;
+
+	// Feedforward pass : For each example individually. Matrix notation faster.
+	// For each of the 10000 values here we get a activation for each layer.
+	// lec notes page 5, top
+	// and page 9, step 1.
+	Mat z2 = W1 * data + cv::repeat(b1, 1, numExamples); // z^(2) = W^(1)x + b^(1). Calculate activation of layer 2, given layer 1 and W1, b1.
+	Mat a2 = sigmoid(z2); // a2 = final output activation of L2
+	Mat z3 = W2 * a2 + cv::repeat(b2, 1, numExamples); // same for L3
+	Mat a3 = sigmoid(z3);
+
+	// Jcost = (0.5 / numExamples)*sum(sum((a3 - data). ^ 2)); % page 6, formula(8) lower
+	Jcost = (0.5f / numExamples) * cv::norm(a3, data, cv::NORM_L2SQR); // page 6, formula (8) lower
+
+	Jweight = 0.5f * (cv::norm(W1, cv::NORM_L2SQR) + cv::norm(W2, cv::NORM_L2SQR)); // regularisation (weight decay term)
+
+	// average activation of each hidden unit(averaged over the training set).Page 14
+	Mat a2RowSum;
+	cv::reduce(a2, a2RowSum, 1, CV_REDUCE_SUM);
+	Mat rho = (1.0f / numExamples) * a2RowSum;
+	// we want rho to be close to sparsityParam, and we'll punish deviations (using the KL div.)
+	// Jsparse = sum(sparsityParam.*log(sparsityParam. / rho) + (1 - sparsityParam).*log((1 - sparsityParam). / (1 - rho))); // sum over all neurons(except bias) of L2
+	Mat logSoverRho, logOneMinusSoverOneMinusRho;
+	cv::log(sparsityParam / rho, logSoverRho);
+	cv::log((1 - sparsityParam) / (1 - rho), logOneMinusSoverOneMinusRho);
+	Jsparse = cv::sum(sparsityParam * logSoverRho + (1 - sparsityParam) * logOneMinusSoverOneMinusRho)[0];
+	
+	float cost = Jcost + lambda * Jweight + beta * Jsparse;
+
+	// Step 2 page 9
+	// d3 = -(data - a3).*sigmoidInv(z3); % delta's of output layer, directly calculated using the labels (or in the AutoEnc (unsup. learning) case, the original data, that's what we want to reconstruct
+	// we don't need sigmoidInv, use the expression mentioned in page9:
+	// d3 = -(data-a3).* (a3.*(1-a3)); % the second whole part in () is f'(z_i^l)
+	Mat d3 = -(data - a3).mul(a3.mul(1 - a3));
+	
+	Mat sterm = beta * (-sparsityParam / rho + (1 - sparsityParam) / (1 - rho));
+	// Step 3 page 9
+	// % d2 = (W2'*d3+repmat(sterm,1,numExamples)).*sigmoidInv(z2); 
+	// d2 = (W2'*d3+repmat(sterm,1,numExamples)).* (a2.*(1-a2)); 
+	// % d2 = (W2'*d3).* (a2.*(1-a2)); % same 'trick' for f'(z)
+	Mat d2 = (W2.t() * d3 + cv::repeat(sterm, 1, numExamples)).mul(a2.mul(1 - a2));
+
+	// Step 4 page 9
+	// W1grad
+	Mat W1grad = d2 * data.t();
+	W1grad = (1.0f / numExamples) * W1grad + lambda * W1;
+
+	// W2grad
+	Mat W2grad = d3 * a2.t();
+	W2grad = (1.0f / numExamples) * W2grad + lambda * W2;
+
+	// b1grad
+	Mat d2RowSum;
+	cv::reduce(d2, d2RowSum, 1, CV_REDUCE_SUM);
+	Mat b1grad = d2RowSum; // sum over each training example, see page 9 bottom 2c.
+	b1grad = (1.0f / numExamples) * b1grad;
+
+	// b2grad
+	Mat d3RowSum;
+	cv::reduce(d3, d3RowSum, 1, CV_REDUCE_SUM);
+	Mat b2grad = d3RowSum;
+	b2grad = (1.0f / numExamples) * b2grad;
+
+	// convert gradients to vector format for the optimiser
+	Mat grad;
+	cv::vconcat(W1grad.reshape(1, hiddenSize * visibleSize), W2grad.reshape(1, hiddenSize * visibleSize), grad);
+	cv::vconcat(grad, b1grad, grad);
+	cv::vconcat(grad, b2grad, grad);
+	return std::make_pair(cost, grad);
 }
 
 int main(int argc, char *argv[])
@@ -271,14 +396,33 @@ int main(int argc, char *argv[])
 
 	// randomly sample patches from the images, i.e. prepare the input to the AE training
 	vector<Mat> features = generatePatches(images, 8, 10000); // input. 8x8 patches, 10000.
-	features = normalizeData(features); // well, some strange normalization to [0.1, 0.9], mean/sdev normalization etc...
+	features = normalizeData(features); // well, some strange normalization to [0.1, 0.9], mean/sdev normalization etc... tiny-cnn seems to do the 0.1, 0.9 thing too? (even 0.8?)
 
 	Mat theta; ///< These are all our weights (including bias-terms) concatenated to a column-vector. 
 	theta = initializeParameters(hiddenSize, visibleSize);
 	// Note: I think we should make a class Parameter (CostParameter?...) that manages the en/un-rolling.
 	// If we keep it like this, we need to keep track of hiddenSize etc.
 
-	float cost, grad;
-	std::tie(cost, grad) = sparseAutoencoderCost(theta, visibleSize, hiddenSize, lambda, sparsityParam, beta, features);
+	// we need the data in matrix form anyway, so here we go:
+	Mat data(features[0].rows * features[0].cols, static_cast<int>(features.size()), CV_32FC1); // each column is 1 patch
+	for (auto i = 0; i < features.size(); ++i) {
+		Mat reshaped = features[i].reshape(1, features[0].rows * features[0].cols);
+		reshaped.copyTo(data.col(i));
+	}
+
+	float cost;
+	Mat grad;
+	std::tie(cost, grad) = sparseAutoencoderCost(theta, visibleSize, hiddenSize, lambda, sparsityParam, beta, data);
+	// Todo: Test this with a smaller model. I.e. 10 training examples, 3 hidden units.
+	//auto test = std::bind(sparseAutoencoderCost, std::placeholders::_1, visibleSize, hiddenSize, lambda, sparsityParam, beta, data);
+	//computeNumericalGradient(test, theta);
+	// Actually we don't need to compute the gradients numerically. We use the ones obtained from the backprop. It's only to test the backprop gradients.
+	// But our bprop gradients should be ~identical to the numerical gradients. Maybe they're off because of float vs double?
+	
+	// Randomly initialise the parameters:
+	theta = initializeParameters(hiddenSize, visibleSize);
+
+	// we need a LBFGS, CG, SGD or LM optimiser now...
+	
 	return 0;
 }
