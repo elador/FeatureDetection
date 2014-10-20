b45a40442592ef8af5bea454d1d0e48513797723
Patrik Huber <patrikhuber@gmail.com>
2014-10-20 14:51:37 +0100

    Added a small app that reads extracted frames & labels from boost format and writes out images in Caffe format

---
+++ frames-labels-to-caffe/frames-labels-to-caffe.cpp
@@ -0,0 +1,154 @@
+/*
+ * frames-labels-to-caffe.cpp
+ *
+ *  Created on: 11.09.2014
+ *      Author: Patrik Huber
+ *
+ * Ideally we'd use video, match against highres stills? (and not the lowres). Because if still are lowres/bad, we could match a
+ * good frame against a bad gallery, which would give a bad score, but it shouldn't, because the frame is good.
+ * Do we have labels for this?
+ * Maybe "sensor_id","stage_id","env_id","illuminant_id" in the files emailed by Ross.
+ *
+ * Example:
+ * train-frameselect-extract-all -q "C:\Users\Patrik\Documents\GitHub\data\PaSC\Web\nd1Fall2010VideoPaSCTrainingSet.xml" -r "Z:\datasets\multiview02\PaSC\training\video" -l "C:\Users\Patrik\Documents\GitHub\data\PaSC\pasc_training_video_pittpatt_detection.txt" -o out
+ *
+ */
+
+#include <chrono>
+#include <memory>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <random>
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+#ifdef WIN32
+	#define BOOST_ALL_DYN_LINK	// Link against the dynamic boost lib. Seems to be necessary because we use /MD, i.e. link to the dynamic CRT.
+	#define BOOST_ALL_NO_LIB	// Don't use the automatic library linking by boost with VS2010 (#pragma ...). Instead, we specify everything in cmake.
+#endif
+#include "boost/program_options.hpp"
+#include "boost/algorithm/string.hpp"
+#include "boost/filesystem.hpp"
+#include "boost/lexical_cast.hpp"
+#include "boost/archive/text_iarchive.hpp"
+#include "boost/archive/text_oarchive.hpp"
+#include "boost/serialization/utility.hpp"
+
+#include "imageio/MatSerialization.hpp"
+#include "facerecognition/pasc.hpp"
+#include "facerecognition/utils.hpp"
+
+#include "logging/LoggerFactory.hpp"
+
+namespace po = boost::program_options;
+namespace fs = boost::filesystem;
+using logging::Logger;
+using logging::LoggerFactory;
+using logging::LogLevel;
+using cv::Mat;
+using boost::filesystem::path;
+using boost::lexical_cast;
+using std::cout;
+using std::endl;
+using std::make_shared;
+using std::shared_ptr;
+using std::vector;
+using std::string;
+
+int main(int argc, char *argv[])
+{
+	#ifdef WIN32
+	//_CrtSetDbgFlag ( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF ); // dump leaks at return
+	//_CrtSetBreakAlloc(287);
+	#endif
+	
+	string verboseLevelConsole;
+	path inputDataFile, inputDataLabels, outputPath;
+
+	try {
+		po::options_description desc("Allowed options");
+		desc.add_options()
+			("help,h",
+				"produce help message")
+			("verbose,v", po::value<string>(&verboseLevelConsole)->implicit_value("DEBUG")->default_value("INFO","show messages with INFO loglevel or below."),
+				"specify the verbosity of the console output: PANIC, ERROR, WARN, INFO, DEBUG or TRACE")
+			("data,d", po::value<path>(&inputDataFile)->required(),
+				"File with patches in boost::serialization text format")
+			("labels,l", po::value<path>(&inputDataLabels)->required(),
+				"File with labels in boost::serialization text format")
+			("output,o", po::value<path>(&outputPath)->default_value("."),
+				"path to an output folder for images and labels")
+		;
+
+		po::variables_map vm;
+		po::store(po::command_line_parser(argc, argv).options(desc).run(), vm); // style(po::command_line_style::unix_style | po::command_line_style::allow_long_disguise)
+		if (vm.count("help")) {
+			cout << "Usage: frames-labels-to-caffe [options]" << std::endl;
+			cout << desc;
+			return EXIT_SUCCESS;
+		}
+		po::notify(vm);
+
+	}
+	catch (po::error& e) {
+		cout << "Error while parsing command-line arguments: " << e.what() << endl;
+		cout << "Use --help to display a list of options." << endl;
+		return EXIT_SUCCESS;
+	}
+
+	LogLevel logLevel;
+	if(boost::iequals(verboseLevelConsole, "PANIC")) logLevel = LogLevel::Panic;
+	else if(boost::iequals(verboseLevelConsole, "ERROR")) logLevel = LogLevel::Error;
+	else if(boost::iequals(verboseLevelConsole, "WARN")) logLevel = LogLevel::Warn;
+	else if(boost::iequals(verboseLevelConsole, "INFO")) logLevel = LogLevel::Info;
+	else if(boost::iequals(verboseLevelConsole, "DEBUG")) logLevel = LogLevel::Debug;
+	else if(boost::iequals(verboseLevelConsole, "TRACE")) logLevel = LogLevel::Trace;
+	else {
+		cout << "Error: Invalid LogLevel." << endl;
+		return EXIT_FAILURE;
+	}
+	
+	Loggers->getLogger("imageio").addAppender(make_shared<logging::ConsoleAppender>(logLevel));
+	Loggers->getLogger("app").addAppender(make_shared<logging::ConsoleAppender>(logLevel));
+	Logger appLogger = Loggers->getLogger("app");
+
+	appLogger.debug("Verbose level for console output: " + logging::logLevelToString(logLevel));
+
+	// Read the video detections metadata (eyes, face-coords):
+	vector<Mat> trainingFrames;
+	vector<float> labels;
+	{
+		std::ifstream ifs(inputDataFile.string());
+		boost::archive::text_iarchive ia(ifs);
+		ia >> trainingFrames;
+	} // archive and stream closed when destructors are called
+	{
+		std::ifstream ifs(inputDataLabels.string());
+		boost::archive::text_iarchive ia(ifs);
+		ia >> labels;
+	} // archive and stream closed when destructors are called
+	
+	if (trainingFrames.size() != labels.size()) {
+		appLogger.error("");
+		return EXIT_FAILURE;
+	}
+
+	// Create the output directory if it doesn't exist yet:
+	if (!fs::exists(outputPath)) {
+		fs::create_directory(outputPath);
+	}
+
+	std::ofstream labelOut((outputPath / "filelist.txt").string());
+	for (auto i = 0; i < trainingFrames.size(); ++i) {
+		auto img = trainingFrames[i];
+		path patchName = outputPath / ("img_" + std::to_string(i) + ".png");
+		cv::imwrite(patchName.string(), trainingFrames[i]);
+		labelOut << patchName.filename().string() << " " << labels[i] << std::endl;
+	}
+	labelOut.close();
+
+	return EXIT_SUCCESS;
+}
