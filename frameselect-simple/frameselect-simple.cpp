7af5b59daba11f53bcc7c6e08c8091afc345bd10
Patrik Huber <patrikhuber@gmail.com>
2014-10-21 14:35:05 +0100

    Put selectFrameSimple in a function. Fixed cv::minMaxIdx bug

--- frameselect-simple/frameselect-simple.cpp
+++ frameselect-simple/frameselect-simple.cpp
@@ -219,6 +219,91 @@ vector<float> getVideoNormalizedCannySharpnessScores(vector<float> sharpnesses)
 	*/
 }
 
+// Might rename to "assessQualitySimple(video...)"
+std::pair<cv::Mat, int> selectFrameSimple(path inputDirectoryVideos, const facerecognition::FaceRecord& video, const vector<facerecognition::PascVideoDetection>& pascVideoDetections)
+{
+	auto logger = Loggers->getLogger("app");
+
+	auto frames = facerecognition::utils::getFrames(inputDirectoryVideos / video.dataPath);
+	vector<float> headBoxSizes;
+	vector<float> ieds;
+	vector<float> yaws;
+	vector<float> sharpnesses;
+	vector<float> laplModif;
+	vector<float> laplVari;
+	vector<int> frameIds;
+	for (int frameNum = 0; frameNum < frames.size(); ++frameNum) {
+		string frameName = facerecognition::getPascFrameName(video.dataPath, frameNum + 1);
+		logger.debug("Starting to process " + frameName);
+		auto landmarks = std::find_if(begin(pascVideoDetections), end(pascVideoDetections), [frameName](const facerecognition::PascVideoDetection& d) { return (d.frame_id == frameName); });
+		// If we were to run it on the training-videos, we'd have to test if we got the eyes and facebox?
+		// The face box is always given. Only the eyes are missing sometimes.
+		// For the test-videos, the whole line is just missing for frames without annotations.
+		if (landmarks == std::end(pascVideoDetections)) {
+			logger.debug("Frame has no PittPatt detections in the metadata file.");
+			continue;
+		}
+		int tlx = landmarks->fcen_x - landmarks->fwidth / 2.0;
+		int tly = landmarks->fcen_y - landmarks->fheight / 2.0;
+		int w = landmarks->fwidth;
+		int h = landmarks->fheight;
+		if (tlx < 0 || tlx + w >= frames[frameNum].cols || tly < 0 || tly + h >= frames[frameNum].rows) {
+			// patch has some regions outside the image
+			logger.debug("Throwing away patch because it goes outside the image bounds.");
+			continue;
+		}
+		cv::Rect roi(tlx, tly, w, h);
+		Mat croppedFace = frames[frameNum](roi);
+
+		headBoxSizes.emplace_back((landmarks->fwidth + landmarks->fheight) / 2.0f);
+		ieds.emplace_back(cv::norm(cv::Vec2f(*landmarks->le_x, *landmarks->le_y), cv::Vec2f(*landmarks->re_x, *landmarks->re_y), cv::NORM_L2));
+		yaws.emplace_back(landmarks->fpose_y);
+		sharpnesses.emplace_back(sharpnessScoreCanny(croppedFace));
+		frameIds.emplace_back(frameNum);
+
+		laplModif.emplace_back(modifiedLaplacian(croppedFace));
+		laplVari.emplace_back(varianceOfLaplacian(croppedFace));
+		/*{
+			cv::imwrite("out/out_" + std::to_string(frameIds.size() - 1) + "_" + std::to_string(frameNum) + ".png", croppedFace);
+			Mat cannyEdges;
+			cv::Canny(croppedFace, cannyEdges, 225.0, 175.0); // threshold1, threshold2
+			int numEdgePixels = cv::countNonZero(cannyEdges); // throws if 0 nonZero? Check first?
+			float sharpness = numEdgePixels * 1000.0f / (cannyEdges.rows * cannyEdges.cols);
+			cv::imwrite("out/out_" + std::to_string(frameIds.size() - 1) + "_" + std::to_string(frameNum) + "_" + std::to_string(sharpness) + ".png", cannyEdges);
+			}*/
+	}
+
+	cv::Mat headBoxScores(getVideoNormalizedHeadBoxScores(headBoxSizes), true); // function returns a temporary, so we need to copy the data
+	cv::Mat interEyeDistanceScores(getVideoNormalizedInterEyeDistanceScores(ieds), true);
+	cv::Mat yawPoseScores(getVideoNormalizedYawPoseScores(yaws), true);
+	cv::Mat cannySharpnessScores(getVideoNormalizedCannySharpnessScores(sharpnesses), true);
+	cv::Mat modifiedLaplacianInFocusScores(getVideoNormalizedCannySharpnessScores(laplModif), true);
+	cv::Mat varianceOfLaplacianInFocusScores(getVideoNormalizedCannySharpnessScores(laplVari), true);
+
+	// Weights:
+	// 0.2 head
+	// 0.1 ied
+	// 0.4 yaw pose
+	// 0.1 canny
+	// 0.1 modified laplace
+	// 0.1 laplace variance
+	cv::Mat scores = 0.2f * headBoxScores + 0.1f * interEyeDistanceScores + 0.4f * yawPoseScores + 0.1f * cannySharpnessScores + 0.1f * modifiedLaplacianInFocusScores + 0.1f * varianceOfLaplacianInFocusScores;
+
+	double minScore, maxScore;
+	int minIdx[2], maxIdx[2];
+	cv::minMaxIdx(scores, &minScore, &maxScore, &minIdx[0], &maxIdx[0]); // can use NULL if argument not needed
+	int idOfBestFrame = frameIds[maxIdx[0]]; // scores is a single column so it will be M x 1, i.e. we need maxIdx[0]
+	cv::Mat bestFrame = frames[idOfBestFrame];
+
+	//int idOfWorstFrame = frameIds[minIdx];
+	//cv::Mat worstFrame = frames[idOfWorstFrame];
+
+	//for (int i = 0; i < frameIds.size(); ++i) {
+	//	cv::imwrite("out/out_" + std::to_string(i) + "_" + std::to_string(frameIds[i]) + ".png", frames[frameIds[i]]);
+	//}
+	return std::make_pair(bestFrame, idOfBestFrame);
+}
+
 int main(int argc, char *argv[])
 {
 	#ifdef WIN32
@@ -326,86 +411,12 @@ int main(int argc, char *argv[])
 			continue;
 		}
 
-		auto frames = facerecognition::utils::getFrames(inputDirectoryVideos / video.dataPath);
-		vector<float> headBoxSizes;
-		vector<float> ieds;
-		vector<float> yaws;
-		vector<float> sharpnesses;
-		vector<float> laplModif;
-		vector<float> laplVari;
-		vector<int> frameIds;
-		for (int frameNum = 0; frameNum < frames.size(); ++frameNum) {
-			string frameName = facerecognition::getPascFrameName(video.dataPath, frameNum + 1);
-			appLogger.debug("Starting to process " + frameName);
-			auto landmarks = std::find_if(begin(pascVideoDetections), end(pascVideoDetections), [frameName](const facerecognition::PascVideoDetection& d) { return (d.frame_id == frameName); });
-			// If we were to run it on the training-videos, we'd have to test if we got the eyes and facebox?
-			// The face box is always given. Only the eyes are missing sometimes.
-			// For the test-videos, the whole line is just missing for frames without annotations.
-			if (landmarks == std::end(pascVideoDetections)) {
-				appLogger.debug("Frame has no PittPatt detections in the metadata file.");
-				continue;
-			}
-			int tlx = landmarks->fcen_x - landmarks->fwidth / 2.0;
-			int tly = landmarks->fcen_y - landmarks->fheight / 2.0;
-			int w = landmarks->fwidth;
-			int h = landmarks->fheight;
-			if (tlx < 0 || tlx + w >= frames[frameNum].cols || tly < 0 || tly + h >= frames[frameNum].rows) {
-				// patch has some regions outside the image
-				appLogger.debug("Throwing away patch because it goes outside the image bounds.");
-				continue;
-			}
-			cv::Rect roi(tlx, tly, w, h);
-			Mat croppedFace = frames[frameNum](roi);
-			
-			headBoxSizes.emplace_back((landmarks->fwidth + landmarks->fheight) / 2.0f);
-			ieds.emplace_back(cv::norm(cv::Vec2f(*landmarks->le_x, *landmarks->le_y), cv::Vec2f(*landmarks->re_x, *landmarks->re_y), cv::NORM_L2));
-			yaws.emplace_back(landmarks->fpose_y);
-			sharpnesses.emplace_back(sharpnessScoreCanny(croppedFace));
-			frameIds.emplace_back(frameNum);
-			
-			laplModif.emplace_back(modifiedLaplacian(croppedFace));
-			laplVari.emplace_back(varianceOfLaplacian(croppedFace));
-			/*{
-				cv::imwrite("out/out_" + std::to_string(frameIds.size() - 1) + "_" + std::to_string(frameNum) + ".png", croppedFace);
-				Mat cannyEdges;
-				cv::Canny(croppedFace, cannyEdges, 225.0, 175.0); // threshold1, threshold2
-				int numEdgePixels = cv::countNonZero(cannyEdges); // throws if 0 nonZero? Check first?
-				float sharpness = numEdgePixels * 1000.0f / (cannyEdges.rows * cannyEdges.cols);
-				cv::imwrite("out/out_" + std::to_string(frameIds.size() - 1) + "_" + std::to_string(frameNum) + "_" + std::to_string(sharpness) + ".png", cannyEdges);
-			}*/
-		}
+		cv::Mat bestFrame;
+		int bestFrameId;
+		std::tie(bestFrame, bestFrameId) = selectFrameSimple(inputDirectoryVideos, video, pascVideoDetections);
 
-		cv::Mat headBoxScores(getVideoNormalizedHeadBoxScores(headBoxSizes), true); // function returns a temporary, so we need to copy the data
-		cv::Mat interEyeDistanceScores(getVideoNormalizedInterEyeDistanceScores(ieds), true);
-		cv::Mat yawPoseScores(getVideoNormalizedYawPoseScores(yaws), true);
-		cv::Mat cannySharpnessScores(getVideoNormalizedCannySharpnessScores(sharpnesses), true);
-		cv::Mat modifiedLaplacianInFocusScores(getVideoNormalizedCannySharpnessScores(laplModif), true);
-		cv::Mat varianceOfLaplacianInFocusScores(getVideoNormalizedCannySharpnessScores(laplVari), true);
-
-		// Weights:
-		// 0.2 head
-		// 0.1 ied
-		// 0.4 yaw pose
-		// 0.1 canny
-		// 0.1 modified laplace
-		// 0.1 laplace variance
-		cv::Mat scores = 0.2f * headBoxScores + 0.1f * interEyeDistanceScores + 0.4f * yawPoseScores + 0.1f * cannySharpnessScores + 0.1f * modifiedLaplacianInFocusScores + 0.1f * varianceOfLaplacianInFocusScores;
-
-		double minScore, maxScore;
-		int minIdx, maxIdx;
-		cv::minMaxIdx(scores, &minScore, &maxScore, &minIdx, &maxIdx);
-		int idOfBestFrame = frameIds[maxIdx];
-		cv::Mat bestFrame = frames[idOfBestFrame];
-
-		//int idOfWorstFrame = frameIds[minIdx];
-		//cv::Mat worstFrame = frames[idOfWorstFrame];
-
-		//for (int i = 0; i < frameIds.size(); ++i) {
-		//	cv::imwrite("out/out_" + std::to_string(i) + "_" + std::to_string(frameIds[i]) + ".png", frames[frameIds[i]]);
-		//}
-		
 		path bestFrameName = outputPath / video.dataPath.stem();
-		bestFrameName.replace_extension(std::to_string(idOfBestFrame + 1) + ".png");
+		bestFrameName.replace_extension(std::to_string(bestFrameId + 1) + ".png");
 		cv::imwrite(bestFrameName.string(), bestFrame); // idOfBestFrame is 0-based, PaSC is 1-based
 
 		appLogger.info("Saved best frame to the filesystem as " + bestFrameName.string() + ".");
