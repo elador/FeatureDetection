9a7df2344abbfaf3b28a41597cdb93c6be8bf006
Patrik Huber <patrikhuber@gmail.com>
2014-11-14 15:22:31 +0000

    Added empty file for superviseddescent-v2 stuff

---
+++ libSupervisedDescent/include/superviseddescent/superviseddescent.hpp
@@ -0,0 +1,24 @@
+/*
+ * superviseddescent.hpp.hpp
+ *
+ *  Created on: 14.11.2014
+ *      Author: Patrik Huber
+ */
+
+#pragma once
+
+#ifndef SUPERVISEDDESCENT_HPP_
+#define SUPERVISEDDESCENT_HPP_
+
+#include "opencv2/core/core.hpp"
+
+namespace superviseddescent {
+
+/**
+ * Stuff for supervised descent v2
+ * Will be moved to separate files / cpp files once finished
+ */
+
+
+} /* namespace superviseddescent */
+#endif /* SUPERVISEDDESCENT_HPP_ */
